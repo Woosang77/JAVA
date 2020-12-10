09f93fb61d8915bc778c3265e82879dfe8021915
Woosang77 <wooju9942@gmail.com>
2020-12-10 17:55:49 +0900

    recent threadpool study

--- secondFile/src/Practice/Lesson/ThreadPart/ThreadPool/blocking/ResultByRunnableExample.java
+++ secondFile/src/Practice/Lesson/ThreadPart/ThreadPool/blocking/ResultByRunnableExample.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class ResultByRunnableExample {
-	private static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 		System.out.println("[작업처리요청]");
